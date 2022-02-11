ad29927cdd4ab5d264d64b323ef463a4d7a1664f
WindowsVistaisCool <kyle@quackings.com>
2022-02-10 21:38:18 -0500

    Minimalize

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -14,66 +14,6 @@ class Listeners(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_raw_reaction_add(self, payload):
-        y = store('rroles.json', None, True)
-        if payload.emoji.id == 815831189696413698:
-            blacklistChannel = self.bot.get_channel(880550264564748368)
-            blacklist = await blacklistChannel.history(limit=100).flatten()
-            for blacklistitem in blacklist:
-                if str(payload.channel_id) == blacklistitem: return
-            check = self.bot.get_channel(880550772021010433)
-            checkHistory = await check.history(limit=500).flatten()
-            # needs to be squished, 10 starboards per messsage or something like that
-            for message in checkHistory:
-                if str(payload.message_id) == message.content: return
-            cat = self.bot.get_channel(880550245841403904)
-            ch = None
-            for channel in cat.channels:
-                if channel.name == str(payload.message_id):
-                    ch = channel
-                    break
-            if ch == None:
-                c = await cat.create_text_channel(str(payload.message_id), reason="New starboard")
-                await c.send(f"{payload.user_id}")
-                return
-            history = await ch.history(limit=1).flatten()
-            newhistory = f"{history[0].content}-{payload.user_id}"
-            await history[0].edit(content=newhistory)
-            members = newhistory.split('-')
-            if len(members) >= 3:
-                starboard = self.bot.get_channel(871491040224378940)
-                messageChannel = self.bot.get_channel(payload.channel_id)
-                message = await messageChannel.fetch_message(payload.message_id)
-                embed = discord.Embed(description=message.content, color=discord.Color.blue())
-                embed.set_author(name=message.author, icon_url=message.author.avatar_url)
-                if len(message.attachments) != 0:
-                    embed.set_image(url=message.attachments[0].url)
-                await starboard.send(embed=embed)
-                await ch.delete(reason="Starboard has 3 reactions")
-                await check.send(f"{payload.message_id}")
-                return
-
-        if str(payload.message_id) in y['rroles']:
-            for msg in y['rroles']:
-                if str(payload.message_id) == msg:
-                    if payload.emoji.name == "✅":
-                        guild = self.bot.get_guild(payload.guild_id)
-                        role = guild.get_role(int(y['rrolesrole'][msg]))
-                        await payload.member.add_roles(role)
-
-    @commands.Cog.listener()
-    async def on_raw_reaction_remove(self, payload):
-        x = store('rroles.json', None, True)
-        if str(payload.message_id) in x['rroles']:
-            for msg in x['rroles']:
-                if str(payload.message_id) == msg:
-                    if payload.emoji.name == "✅":
-                        guild = self.bot.get_guild(payload.guild_id)
-                        member = await guild.fetch_member(payload.user_id)
-                        role = guild.get_role(int(x['rrolesrole'][msg]))
-                        await member.remove_roles(role)
-
-    @commands.Cog.listener()
     async def on_button_click(self, interaction):
         try:
                 label = interaction.component.label
@@ -166,86 +106,9 @@ class Listeners(commands.Cog):
                 await interaction.message.edit(components=[])
                 return
             await f.edit(content="Application denied")
-            # add feature to get embed and change it into
             await interaction.message.edit(components=[])
-        elif label == "Vote Yes":
-            ide = ide.replace('POLLYES', '')
-            cat = self.bot.get_channel(886767483342696490)
-            c = None
-            for channel in cat.channels:
-                if channel.name == ide:
-                    c = channel
-                    break
-            if c == None:
-                await interaction.respond(content="Sorry, the poll ID was not found! Try again later or contact trngl.")
-                return
-            messages = await c.history(limit=4).flatten()
-            x = json.loads(messages[1].content)
-            users = []
-            if messages[0].content != "No user data":
-                users = messages[0].content.split('-')
-            try:
-                if str(interaction.user.id) in users:
-                    await interaction.respond(content="You have already responded to this poll!")
-                    return
-                x['yes'] = int(x['yes'])
-                x['yes'] += 1
-                e = interaction.message.embeds[0]
-                e.set_field_at(0, name='Yes', value=x['yes'])
-                await interaction.message.edit(embed=e)
-                if messages[0].content != "No user data":
-                    await messages[0].edit(content=f"{messages[0].content}-{interaction.user.id}")
-                else:
-                    await messages[0].edit(content=f"{interaction.user.id}")
-                await messages[1].edit(content=f"{json.dumps(x)}")
-                await interaction.respond(type=6)
-            except:
-                await interaction.respond(content="There was an error executing this!")
-        elif label == "Vote No":
-            ide = ide.replace('POLLNO', '')
-            cat = self.bot.get_channel(886767483342696490)
-            c = None
-            for channel in cat.channels:
-                if channel.name == ide:
-                    c = channel
-                    break
-            if c == None:
-                await interaction.respond(content="Sorry, the poll ID was not found! Try again later or contact rctngl.")
-                return
-            messages = await c.history(limit=4).flatten()
-            x = json.loads(messages[1].content)
-            users = []
-            if messages[0].content != "No user data":
-                users = messages[0].content.split('-')
-            try:
-                if str(interaction.user.id) in users:
-                    await interaction.respond(content="You have already responded to this poll!")
-                    return
-                x['no'] = int(x['no'])
-                x['no'] += 1
-                e = interaction.message.embeds[0]
-                e.set_field_at(1, name='No', value=x['no'])
-                await interaction.message.edit(embed=e)
-                if messages[0].content != "No user data":
-                    await messages[0].edit(content=f"{messages[0].content}-{interaction.user.id}")
-                else:
-                    await messages[0].edit(content=f"{interaction.user.id}")
-                await messages[1].edit(content=f"{json.dumps(x)}")
-                await interaction.respond(type=6)
-            except:
-                await interaction.respond(content="There was an error executing this!")
         elif label == "Remove" or ide == "remove":
             await interaction.message.delete()
-        try:
-            if ide == "deverify":
-                role = interaction.message.guild.get_role(788914323485491232)
-                mrole = interaction.message.guild.get_role(788890991028469792)
-                member = await interaction.message.guild.fetch_member(interaction.user.id)
-                await member.add_roles(mrole)
-                await member.remove_roles(role)
-                await interaction.respond(type=7, content=f"LMFAOOOOOO L L L L {member.name} JUST GOT CLWOON'd ON L L L L L L L GET L KID")
-        except:
-            pass
         try:
             userid = ide.split('-')[0]
         except:
@@ -255,53 +118,6 @@ class Listeners(commands.Cog):
             await interaction.message.edit(components=[])
         elif label == 'Delete' or ide == "delete" or interaction.user.id == 406629388059410434:
             await interaction.message.delete()
-
-    @commands.Cog.listener()
-    async def on_select_option(self, interaction):
-        val = ''
-        try:
-            val = interaction.values[0]
-            if interaction.component.id == "nolistener": return
-            label = val.split('-')[4]
-            # label = interaction.label
-        except Exception as e:
-            print(f"error setting id {e}")
-        if val.startswith("SelectPoll"):
-            val = val.split('-')
-            mid = val[1]
-            cat = self.bot.get_channel(886767483342696490)
-            c = None
-            for channel in cat.channels:
-                if channel.name == mid:
-                    c = channel
-                    break
-            if c == None:
-                await interaction.respond(content="Sorry, the poll ID was not found! Try again later or contact rctngl.")
-                return
-            messages = await c.history(limit=4).flatten()
-            x = json.loads(messages[1].content)
-            users = []
-            if messages[0].content != "No user data":
-                users = messages[0].content.split('-')
-            try:
-                if str(interaction.user.id) in users:
-                    await interaction.respond(content="You have already responed to this poll!")
-                    return
-                if messages[0].content != "No user data":
-                    await messages[0].edit(content=f"{messages[0].content}-{interaction.user.id}")
-                else:
-                    await messages[0].edit(content=f"{interaction.user.id}")
-                x['fields'][label] = int(x['fields'][label])
-                x['fields'][label] += 1
-                await messages[1].edit(content=f"{json.dumps(x)}")
-                if val[2] != 'T':
-                    e = interaction.message.embeds[0]
-                    e.set_field_at(x['fieldpos'][label], name=label, value=x['fields'][label], inline=False)
-                    await interaction.message.edit(embed=e)
-                await interaction.respond(type=6)
-            except Exception as e:
-                await interaction.respond(content="There was an error executing this!")
-                print(f"{e}")
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -351,40 +167,6 @@ class Listeners(commands.Cog):
                 if command == ctx.command:
                     return
         except: pass
-        
-
-    @commands.Cog.listener()
-    async def on_message_delete(self, message):
-        if message.author.bot or message.content.startswith('='): return
-        d = store('expose.json', None, True)
-        files = []
-        if message.attachments != []:
-            for file in message.attachments:
-                files.append(file.url)
-        if message.mentions == []: ghost = False
-        else: ghost = [person.id for person in message.mentions]
-        d[str(message.channel.id)] = {
-            "type": "delete",
-            "content": message.content,
-            "author": f"{message.author}",
-            "author_icon": f"{message.author.avatar_url}",
-            "ghostping": ghost,
-            "files": files
-        }
-        store('expose.json', d)
-
-    @commands.Cog.listener()
-    async def on_message_edit(self, before, after):
-        d = store('expose.json', None, True)
-        if before.mentions != after.mentions: ghost = True
-        else: ghost = [person.id for person in before.mentions]
-        d[str(after.channel.id)] = {
-            "type": "edit",
-            "content": before.content,
-            "ghostping": ghost,
-            "files": []
-        }
-        store('expose.json', d)
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
